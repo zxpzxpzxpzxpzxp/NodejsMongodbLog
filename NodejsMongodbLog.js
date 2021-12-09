c9bc5e7f77ecba949052e2df6f8a0076c071c875
zxp <111.com>
2021-12-08 17:03:28 -0800

    express successfully

---
+++ NodejsMongodbLog.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app=express()
+app.use('/',express.static('public'))
+app.get("/input",(req,res)=>{
+    res.send(req.query)
+    console.log(req.query)
+})
+app.listen(10435)
