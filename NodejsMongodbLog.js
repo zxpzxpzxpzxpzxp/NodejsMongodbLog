9215478cc2fe0d7bb3cfd2f55d5da547320a66ed
zxp <111.com>
2021-12-08 17:38:38 -0800

    mongoose successfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,17 @@
 const express = require("express")
 const app=express()
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test1')
+const schema = {
+    name: String,
+    age: Number,
+    health: String,
+    hobby:String
+}
+const mydata = mongoose.model('cat1s', schema);
+const kitty = new mydata({ name: 'Zildjian2' });
+kitty1.save()
+
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
