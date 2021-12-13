889d809ca9e78e259f303f5bf0349614648d7fdc
zxp <111.com>
2021-12-12 17:25:29 -0800

    mongodb successful control

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app=express()
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test1')
+mongoose.connect('mongodb://172.21.2.236:27017/190110910435')
 const schema = {
     name: String,
     age: Number,
@@ -9,12 +9,14 @@ const schema = {
     hobby:String
 }
 const mydata = mongoose.model('cat1s', schema);
-const kitty = new mydata({ name: 'Zildjian2' });
-kitty.save()
+// const kitty = new mydata({ name: 'Zildjian2' });
+// kitty.save()
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+    const kitty = new mydata({ name:req.query.first,health:req.query.second});
+    kitty.save()
 })
 app.listen(10435)
